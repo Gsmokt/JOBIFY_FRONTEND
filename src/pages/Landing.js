e4f806af8f6ecfdbf08929f585ba889511a5ebd9
Grzegorz Smoktunowicz <biarth@wp.pl>
2023-03-09 23:12:10 +0100

    first commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
+import main from "../assets/images/main-alternative.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>Start your adventure with the Jobify App!</p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
